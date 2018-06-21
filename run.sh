517c75d38abf7dd3cdb28efeddfde12caff1623a
Uksusov Kirill <uksusoff@yandex.ru>
2018-06-21 11:46:42 +0400

    Initial commit.

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+
+wget https://www.adminer.org/latest.php -O adminer.php
+wget https://raw.githubusercontent.com/vrana/adminer/master/designs/nette/adminer.css -O adminer.css
+wget https://raw.github.com/vrana/adminer/master/plugins/plugin.php -P plugins
+wget https://raw.github.com/vrana/adminer/master/plugins/dump-date.php -P plugins
