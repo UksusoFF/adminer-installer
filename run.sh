b92da01c7b676551b0fcb369e3bf2a5780ba61ad
Uksusov Kirill <uksusoff@gmail.com>
2021-08-19 15:01:35 +0400

    wget replaced with curl

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ mkdir -p "${PLUGIN_DIR}"
 
 curl -L https://www.adminer.org/latest.php > "${SCRIPT_DIR}/adminer.php"
 curl https://raw.githubusercontent.com/vrana/adminer/master/designs/nette/adminer.css > "${SCRIPT_DIR}/adminer.css"
-curl https://raw.github.com/vrana/adminer/master/plugins/plugin.php > "${PLUGIN_DIR}/plugin.php"
-curl https://raw.github.com/vrana/adminer/master/plugins/dump-date.php > "${PLUGIN_DIR}/dump-date.php"
+curl https://raw.githubusercontent.com/vrana/adminer/master/plugins/plugin.php > "${PLUGIN_DIR}/plugin.php"
+curl https://raw.githubusercontent.com/vrana/adminer/master/plugins/dump-date.php > "${PLUGIN_DIR}/dump-date.php"
 
 if [ "$THEME" == "dark" ]; then
   cat "adminer-dark.css" >> "adminer.css"
